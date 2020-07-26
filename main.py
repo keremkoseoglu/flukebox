da4cc93c7724c720a67ef10268b1b72e4ddbf73e
keremkoseoglu <kerem@keremkoseoglu.com>
2020-07-26 15:52:41 +0300

    youtube update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 """ Entry point """
 from spotify_test import test1
 print("hello world!")
+# youtube api key: AIzaSyA2pg76E1suANvoIfamehV7ssoV-shCK5I
+# youtube sample call: https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyA2pg76E1suANvoIfamehV7ssoV-shCK5I&playlistId=PLES3BCUQLP6hBBIODciynJr7VAgXSbonF
 test1()
