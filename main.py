8aa33dca36490d9ac2172816a3a7b9e81994994e
keremkoseoglu <kerem@keremkoseoglu.com>
2020-07-26 16:03:25 +0300

    youtube

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from spotify_test import test1
 print("hello world!")
 # youtube api key: AIzaSyA2pg76E1suANvoIfamehV7ssoV-shCK5I
 # youtube sample call: https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyA2pg76E1suANvoIfamehV7ssoV-shCK5I&playlistId=PLES3BCUQLP6hBBIODciynJr7VAgXSbonF
+# youtube sample call 2: https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyA2pg76E1suANvoIfamehV7ssoV-shCK5I&playlistId=PLES3BCUQLP6hBBIODciynJr7VAgXSbonF&part=contentDetails
 test1()
