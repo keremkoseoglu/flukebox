ed8c6fea8fa157deff033405462c40c97955a180
keremkoseoglu <kerem@keremkoseoglu.com>
2020-07-30 14:46:58 +0300

    command line - playlist name Fixes #3

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 """ Entry point """
 import os
+import sys
 from PyQt5.Qt import QApplication
 from flukebox.gui.prime import Prime
+from flukebox.production.producer import Producer
 
-APP = QApplication([])
-P = Prime()
-os._exit(APP.exec_()) # pylint: disable=W0212
+
+def main():
+    arg_pos = -1
+    for arg in sys.argv:
+        arg_pos += 1
+        if arg_pos == 0:
+            continue
+        arg_split = arg.split("=")
+        if len(arg_split) < 2:
+            continue
+        if arg_split[0] == "playlist":
+            Producer().produce_with_playlist(arg_split[1])
+            return
+
+    APP = QApplication([])
+    P = Prime()
+    os._exit(APP.exec_()) # pylint: disable=W0212
+
+if __name__ == "__main__":
+    main()
